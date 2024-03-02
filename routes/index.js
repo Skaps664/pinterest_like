067cce330310e32f063e6f36e19165bbad10ce47
Skaps664 <msudaisk664@gmail.com>
2024-03-02 16:18:23 +0500

    login signup frontend and backend setted up

--- routes/index.js
+++ routes/index.js
@@ -2,30 +2,82 @@ var express = require("express");
 var router = express.Router();
 const userModel = require("../models/userModel");
 const postModel = require("../models/postModel");
+var passport = require("passport");
+const localStrategy = require("passport-local");
+
+passport.use(new localStrategy(userModel.authenticate()));
 
 router.get("/", function (req, res) {
-  res.render("index", { title: "LEARNING" });
+  res.render("index", { title: "Pinterest" });
+});
+router.get("/signup", function (req, res) {
+  res.render("signup");
+});
+router.get("/login", function (req, res) {
+  res.render("login");
 });
 
-router.get("/createuser", async function (req, res, next) {
-  let createdUser = await userModel.create({
-    username: "Skaps",
-    password: "hehe",
-    posts: [],
-    email: "msudaisk664@gmail.com",
-    fullName: "Omer khan",
-  });
-  res.send(createdUser);
+router.get("/profile", isLoggedIn, function (req, res, next) {
+  res.send("profile");
 });
 
-router.get("/createpost", async function (req, res, next) {
-  let createdPost = await postModel.create({
-    postText: "This is first test post",
-    user: "65e2cd63d5f1c71c7b25c84e",
+// router.get("/createuser", async function (req, res, next) {
+//   let createdUser = await userModel.create({
+//     username: "Skaps",
+//     password: "hehe",
+//     posts: [],
+//     email: "msudaisk664@gmail.com",
+//     fullName: "Omer khan",
+//   });
+//   res.send(createdUser);
+// });
+
+// router.get("/createpost", async function (req, res, next) {
+//   let createdPost = await postModel.create({
+//     postText: "This is first test post",
+//     user: "65e2cd63d5f1c71c7b25c84e",
+//   });
+//   let user = await userModel.findOne({ _id: "65e2cd63d5f1c71c7b25c84e" });
+//   user.posts.push(createdPost._id);
+//   user.save();
+//   res.send("done");
+// });
+
+router.post("/signup", function (req, res) {
+  const { username, email, fullName } = req.body;
+  const userData = new userModel({
+    username,
+    email,
+    fullName,
   });
-  let user = await userModel.findOne({ _id: "65e2cd63d5f1c71c7b25c84e" });
-  user.posts.push(createdPost._id);
-  user.save();
-  res.send("done");
+  userModel.register(userData, req.body.password).then(function () {
+    passport.authenticate("local")(req, res, function () {
+      res.redirect("/profile");
+    });
+  });
 });
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/",
+  }),
+  function (req, res) {}
+);
+
+router.get("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/");
+}
+
 module.exports = router;
